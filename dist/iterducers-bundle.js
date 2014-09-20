f2b1416f1722c1f8f0e2ac908c7a02dddde09729
Patrick Dubroy <pdubroy@gmail.com>
2014-09-20 21:47:40 +0200

    Working implementations of map, filter, takeWhile, and compose.

--- dist/iterducers-bundle.js
+++ dist/iterducers-bundle.js
@@ -1,34 +1,33 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.iterducers=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
 var ArrayProto = Array.prototype;
-
 function Transducer(nextFn) {
   this._next = nextFn;
 }
-
-// Returns an iterator whose values are the result of applying the
-// to transducer to `iterable`.
 Transducer.prototype.from = function(iterable) {
   var iter = iterable[Symbol.iterator]();
-  var result = {
-    next: function() {
+  var result = {next: function() {
       return this._next(iter);
-    }.bind(this)
+    }.bind(this)};
+  result[Symbol.iterator] = function() {
+    return result;
   };
-  result[Symbol.iterator] = function() { return result; }
   return result;
 };
-
 Transducer.prototype.into = function(dest, iterable) {
   var i = this.from(iterable);
-  for (var x of this.from(iterable)) {
-    dest.push(x);
+  for (var $__0 = this.from(iterable)[Symbol.iterator](),
+      $__1; !($__1 = $__0.next()).done; ) {
+    var x = $__1.value;
+    {
+      dest.push(x);
+    }
   }
   return dest;
 };
-
 Transducer.prototype.toArray = function(iterable) {
   return this.into([], iterable);
 };
-
 function map(fn) {
   return new Transducer(function(iter) {
     var result = iter.next();
@@ -37,7 +36,6 @@ function map(fn) {
     return result;
   });
 }
-
 function filter(fn) {
   return new Transducer(function(iter) {
     while (true) {
@@ -47,16 +45,14 @@ function filter(fn) {
     }
   });
 }
-
 function takeWhile(fn) {
-  return new Transducer(function(iter) {
-    var result = iter.next();
+  return new Transducer(function() {
+    var result = getNext(iterable);
     if (result.done || fn(result.value))
       return result;
-    return { done: true };
+    return {done: true};
   });
 }
-
 function compose() {
   if (arguments.length > 1) {
     var pair = ArrayProto.slice.call(arguments, -2);
@@ -68,10 +64,12 @@ function compose() {
   }
   return arguments[0];
 }
-
 module.exports = {
   compose: compose,
   map: map,
-  filter: filter,
-  takeWhile: takeWhile
+  filter: filter
 };
+
+
+},{}]},{},[1])(1)
+});
--- test/test-iterducers.js
+++ test/test-iterducers.js
@@ -1,5 +1,28 @@
 var test = require('tape');
 
-test('example', function (t) {
+var iterducers = require('../');
+
+test('map, filter, compose', function (t) {
+  var twice = iterducers.map(function(i) { return i * 2; });
+  t.deepEqual(twice.toArray([1, 2, 3]), [2, 4, 6]);
+
+  var addOne = iterducers.map(function(i) { return i + 1; });
+  var proc = iterducers.compose(addOne, twice);
+  t.deepEqual(proc.toArray([1, 2, 3]), [3, 5, 7]);
+
+  var even = iterducers.filter(function(i) { return i % 2 === 0; });
+  t.deepEqual(even.toArray([1, 2, 3]), [2]);
+
+  t.ok(iterducers.compose() === undefined, 'composing nothing is undefined');
+  proc = iterducers.compose(twice, even, addOne);
+  t.deepEqual(proc.toArray([1, 2, 3]), [4, 8]);
+  proc = iterducers.compose(twice);
+  t.deepEqual(proc.toArray([1, 2, 3]), twice.toArray([1, 2, 3]));
+
+  var lessThanFive = iterducers.takeWhile(function(i) { return i < 5; });
+  t.deepEqual(lessThanFive.toArray([5, 1]), []);
+  t.deepEqual(lessThanFive.toArray([1, 2, 5]), [1, 2]);
+  t.deepEqual(lessThanFive.toArray([1, 2, 5, 1]), [1, 2]);
+
   t.end();
 });
